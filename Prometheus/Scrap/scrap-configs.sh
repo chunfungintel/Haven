efd435f3db4f29969561d9d8a44676d94d54fa02
Leow Chun Fung <chun.fung.leow@intel.com>
2022-07-05 09:20:05 +0800

    Update

--- Prometheus/Scrap/scrap-configs.sh
+++ Prometheus/Scrap/scrap-configs.sh
@@ -2,3 +2,5 @@
 
 kubectl create secret generic additional-scrape-configs --from-file=prometheus-additional.yaml --dry-run=client -oyaml | tee additional-scrape-configs.yaml
 
+kubectl --insecure-skip-tls-verify apply -n prometheus -f additional-scrape-configs.yaml
+
